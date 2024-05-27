a161c91711c5db7d7bcce4b8bd5481af73b50c59
MinChangJeong <wjdalsckd777@naver.com>
2024-05-27 21:22:04 +0900

    init ms project

---
+++ api/product-service/src/test/java/backend/api/product/ProductServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package backend.api.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ api/review-service/src/test/java/backend/api/review/ReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package backend.api.review;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
